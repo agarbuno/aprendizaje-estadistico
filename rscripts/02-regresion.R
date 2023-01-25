6c280475bcc456bea767815c669ea128996199f7
agarbuno <alfredo.garbuno@itam.mx>
2023-01-24 20:31:23 -0600

    updates script

--- rscripts/02-regresion.R
+++ rscripts/02-regresion.R
@@ -122,3 +122,109 @@ data |>
   ggplot(aes(Income, Balance, group = Student, color = Student)) +
   geom_smooth(method = "lm", se = FALSE) + 
   geom_point()
+
+## Aplicación: Modelo de regresión asistencia partidos -----------------------
+
+library(tidyverse)
+base_url <- "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/"
+
+attendance <- read_csv(paste(base_url, "attendance.csv", sep = ""),
+                       progress = FALSE, show_col_types = FALSE)
+standings <- read_csv(paste(base_url, "standings.csv", sep = ""),
+                      progress = FALSE, show_col_types = FALSE)
+
+attendance_joined <- attendance |>
+  left_join(standings, by = c("year", "team_name", "team"))
+attendance_joined
+
+attendance_joined |>
+  filter(!is.na(weekly_attendance)) |>
+  ggplot(aes(fct_reorder(team_name, weekly_attendance),
+             weekly_attendance,
+             fill = playoffs)) +
+  geom_boxplot(outlier.alpha = 0.5) +
+  labs(
+    fill = NULL, x = NULL,
+    y = "Weekly NFL game attendance"
+  ) + sin_lineas + theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))
+
+attendance_joined |>
+  distinct(team_name, year, margin_of_victory, playoffs) |>
+  ggplot(aes(margin_of_victory, fill = playoffs)) +
+  geom_histogram(position = "identity", alpha = 0.7) +
+  labs(
+    x = "Margin of victory",
+    y = "Number of teams",
+    fill = NULL
+  ) + sin_lineas
+
+attendance_joined |>
+  mutate(week = factor(week)) |>
+  ggplot(aes(week, weekly_attendance, fill = week)) +
+  geom_boxplot(show.legend = FALSE, outlier.alpha = 0.5) +
+  labs(
+    x = "Week of NFL season",
+    y = "Weekly NFL game attendance"
+  ) + sin_lineas
+
+attendance_df <- attendance_joined |>
+  filter(!is.na(weekly_attendance)) |>
+  select(
+    weekly_attendance, team_name, year, week,
+    margin_of_victory, strength_of_schedule, playoffs
+  )
+
+attendance_df
+
+library(tidymodels)
+
+set.seed(108727)
+attendance_split <- attendance_df |>
+  initial_split(strata = playoffs)
+
+nfl_train <- training(attendance_split)
+nfl_test <- testing(attendance_split)
+
+lm_spec <- linear_reg() |>
+  set_engine(engine = "lm")
+
+lm_spec
+
+lm_fit <- lm_spec |>
+  fit(weekly_attendance ~ .,
+      data = nfl_train
+      )
+
+lm_fit |> broom::tidy()
+
+lm_fit |> broom::tidy(conf.int = TRUE)
+
+lm_fit |> broom::glance()
+
+results_train <- lm_fit |>
+  predict(new_data = nfl_train) |>
+  mutate(truth = nfl_train$weekly_attendance,
+         model = "lm")
+results_train |>
+  rmse(truth = truth, estimate = .pred)
+
+results_test <- lm_fit |>
+  predict(new_data = nfl_test) |>
+  mutate(truth = nfl_test$weekly_attendance,
+         model = "lm")
+results_test |>
+  rmse(truth = truth, estimate = .pred)
+
+results_test %>%
+  mutate(train = "testing") %>%
+  bind_rows(results_train %>%
+    mutate(train = "training")) %>%
+  ggplot(aes(truth, .pred)) +
+  geom_point(alpha = 0.5) +
+  geom_abline(lty = 2, color = "salmon", size = 1.5) +
+  facet_wrap(~train) +
+  labs(
+    x = "Truth",
+    y = "Predicted attendance",
+    color = "Type of model"
+  ) + sin_lineas
