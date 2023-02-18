fe889f4550510d917616d173f57664af1a6ebde4
agarbuno <alfredo.garbuno@itam.mx>
2023-02-17 20:15:29 -0600

    Actualiza scripts (falta notas)

--- rscripts/04-remuestreo.R
+++ rscripts/04-remuestreo.R
@@ -109,3 +109,44 @@ g.cv <- data |>
     ylim(16, 26)+ sin_lineas
 
   g.loo + g.cv
+
+
+## Ejemplo --------------------------------------------------------------------
+## Objetivo: Predicciones, multisplits
+
+library(tidymodels)
+data(ames)
+glimpse(ames)
+
+ames <- mutate(ames, Sale_Price = log10(Sale_Price))
+
+set.seed(502)
+ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+ames_rec <- 
+  recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type + 
+           Latitude + Longitude, data = ames_train) %>%
+  step_log(Gr_Liv_Area, base = 10) %>% 
+  step_other(Neighborhood, threshold = 0.01) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_interact( ~ Gr_Liv_Area:starts_with("Bldg_Type_") ) %>% 
+  step_ns(Latitude, Longitude, deg_free = 20)
+
+lm_model <- linear_reg() %>% set_engine("lm")
+
+lm_wflow <- 
+  workflow() %>% 
+  add_model(lm_model) %>% 
+  add_recipe(ames_rec)
+
+lm_fit <- fit(lm_wflow, ames_train)
+
+set.seed(1001)
+ames_folds <- vfold_cv(ames_train, v = 10)
+
+control_fit <- control_resamples(verbose = TRUE)
+
+set.seed(1003)
+lm_res <- lm_wflow %>% fit_resamples(resamples = ames_folds, control = control_fit)
