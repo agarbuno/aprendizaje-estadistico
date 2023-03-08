a3fd98cc0360e6275a15aaeb0dd0902d5c75fb10
agarbuno <alfredo.garbuno@itam.mx>
2023-03-08 11:50:50 -0600

    agrega ejemplo de the office (regularizacion)

--- rscripts/05-regularizacion.R
+++ rscripts/05-regularizacion.R
@@ -296,3 +296,141 @@ g2 <- cv.results |>
   ylab("Error Validación Cruzada") +
   scale_x_log10()
 g2
+
+## Aplicación: The Office ----------------------------------------------------
+
+ratings_raw <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv", show_col_types = FALSE)
+
+  remove_regex <- "[:punct:]|[:digit:]|parts |part |the |and"
+
+  office_ratings <- ratings_raw |>
+    transmute(
+      episode_name = str_to_lower(title),
+      episode_name = str_remove_all(episode_name, remove_regex),
+      episode_name = str_trim(episode_name),
+      imdb_rating
+    )
+
+  office_info <- schrute::theoffice |>
+    mutate(
+      season = as.numeric(season),
+      episode = as.numeric(episode),
+      episode_name = str_to_lower(episode_name),
+      episode_name = str_remove_all(episode_name, remove_regex),
+      episode_name = str_trim(episode_name)
+    ) |>
+    select(season, episode, episode_name, director, writer, character)
+
+characters <- office_info %>%
+  count(episode_name, character) %>%
+  add_count(character, wt = n, name = "character_count") %>%
+  filter(character_count > 800) %>%
+  select(-character_count) %>%
+  pivot_wider(
+    names_from = character,
+    values_from = n,
+    values_fill = list(n = 0)
+  )
+
+characters |> print(n = 5)
+
+creators <- office_info %>%
+  distinct(episode_name, director, writer) %>%
+  pivot_longer(director:writer, names_to = "role", values_to = "person") %>%
+  separate_rows(person, sep = ";") %>%
+  add_count(person) %>%
+  filter(n > 10) %>%
+  distinct(episode_name, person) %>%
+  mutate(person_value = 1) %>%
+  pivot_wider(
+    names_from = person,
+    values_from = person_value,
+    values_fill = list(person_value = 0)
+  )
+
+creators |> print(n = 5)
+
+office <- office_info %>%
+  distinct(season, episode, episode_name) %>%
+  inner_join(characters) %>%
+  inner_join(creators) %>%
+  inner_join(office_ratings %>%
+             select(episode_name, imdb_rating)) %>%
+  janitor::clean_names()
+
+office |> print(n = 5)
+
+office %>%
+  ggplot(aes(episode, imdb_rating, fill = as.factor(episode))) +
+  geom_boxplot(show.legend = FALSE) + sin_lineas
+
+office_split <- initial_split(office, strata = season)
+office_train <- training(office_split)
+office_test <- testing(office_split)
+
+office_rec <- recipe(imdb_rating ~ ., data = office_train) |>
+  update_role(episode_name, new_role = "ID") |>
+  step_zv(all_numeric_predictors()) |>
+  step_normalize(all_numeric_predictors())
+
+office_prep <- office_rec |>
+  prep(strings_as_factors = FALSE)
+
+set.seed(108727)
+office_boot <- vfold_cv(office_train, v = 10, strata = season)
+
+set.seed(2020)
+wf <- workflow() |>
+  add_recipe(office_rec)
+
+lasso_grid <- tune_grid(
+  wf |> add_model(tune_spec),
+  resamples = office_boot,
+  grid = lambda_grid,
+  control = control_grid(verbose = FALSE)
+)
+
+lasso_grid |>
+  collect_metrics()
+
+lasso_grid %>%
+  collect_metrics() %>%
+  ggplot(aes(penalty, mean, color = .metric)) +
+  geom_linerange(aes(
+    ymin = mean - std_err,
+    ymax = mean + std_err
+  ),
+  alpha = 0.5
+  ) +
+  geom_line(size = 1.5) + geom_point() + 
+  facet_wrap(~.metric, scales = "free", nrow = 2) +
+  scale_x_log10() +
+  theme(legend.position = "none") + sin_lineas
+
+lowest_rmse <- lasso_grid |>
+  select_best("rmse")
+
+final_lasso <- finalize_workflow(
+  wf |> add_model(tune_spec),
+  lowest_rmse
+)
+
+library(vip)
+
+final_lasso %>%
+  fit(office_train) %>%
+  pull_workflow_fit() %>%
+  vi(lambda = lowest_rmse$penalty) %>%
+  mutate(
+    Importance = abs(Importance),
+    Variable = fct_reorder(Variable, Importance)
+  ) %>%
+  ggplot(aes(x = Importance, y = Variable, fill = Sign)) +
+  geom_col() +
+  scale_x_continuous(expand = c(0, 0)) +
+  labs(y = NULL) + sin_lineas
+
+final_lasso |>
+  fit(office_train) |>
+  augment(new_data = office_test) |>
+  rmse(truth = imdb_rating, estimate = .pred)
