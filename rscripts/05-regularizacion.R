2fe1934f2a3c644fe212707b6759ec525e82c1f4
agarbuno <alfredo.garbuno@itam.mx>
2023-03-09 09:45:26 -0600

    elimina codigo redundante

--- rscripts/05-regularizacion.R
+++ rscripts/05-regularizacion.R
@@ -218,108 +218,29 @@ ridge_final_fit <- fit(ridge_final, data = data_train)
 augment(ridge_final_fit, new_data = data_test) |>
   rmse(truth = Balance, estimate = .pred)
 
-## LASSO -------------------------------------
-library(glmnet)
-library(recipes)
-
-atributos <- model.matrix(Balance ~ . - 1, data)
-respuesta <- data |> pull(Balance)
-
-separa_procesa <- function(split){
-  ## Separa datos
-  train <- analysis(split)
-  valid <- assessment(split)
-  ## Preparo objetivo 
-  rec <- recipe(respuesta ~ .,  data = train)
-  ## Defino procesamiento de datos
-  estandarizador <- rec |>
-    step_normalize(Income, Limit, Rating, Cards, Age, Education, respuesta)
-  ## Calculo medias y desviaciones estandar en entrenamiento
-  estandarizador.ajustado <- prep(estandarizador, train)
-  ##
-  valid.std <- bake(estandarizador.ajustado, valid)
-  train.std <- bake(estandarizador.ajustado, train)
-  list(train = train.std, valid = valid.std)
-}
-
-ajusta_lasso <- function(split){
-## Separo en entrenamiento / validacion
-split <- separa_procesa(split)
-## Extraigo atributos / respuesta 
-xtrain <- split$train |> select(-respuesta) |> as.matrix()
-ytrain <- split$train |> pull(respuesta) |> as.matrix()
-xvalid <- split$valid |> select(-respuesta) |> as.matrix()
-yvalid <- split$valid |> pull(respuesta) |> as.matrix()
-
-## Ajusta modelos para trayectoria de lambda
-tibble(lambda = 10**seq(-4, 2, length.out = 50)) |>
-  mutate(modelo = map(lambda, function(lam){
-    ## Ajusto modelo con lambda fija
-    glmnet(y = ytrain, x = xtrain, lambda = lam, alpha = 1)
-  }), residuales = map(modelo, function(mod){
-    ## Calculo residuales 
-    predict(mod, newx = xvalid) - yvalid
-  }))
-}
-
-cv.results <- cbind(atributos, respuesta) |>
-  as_tibble() |>
-  vfold_cv(10) |>
-  mutate(results = map(splits, ajusta_lasso))
-
-g1 <- cv.results |>
-  filter(id == "Fold01") |>
-  unnest(results) |>
-  mutate(estimates = map(modelo, tidy)) |>
-  select(-lambda) |>
-  unnest(estimates) |>
-  filter(term != "(Intercept)") |>
-  complete(term, lambda, fill = list(estimate = 0)) |>
-  ggplot(aes(lambda, estimate, color= term)) + sin_lineas +
-  geom_line() + scale_x_log10() + xlab(expression(lambda))
-g1
-
-g2 <- cv.results |>
-  unnest(results) |>
-  mutate(valid.error = map_dbl(residuales, function(x){mean(x**2)})) |>
-  group_by(lambda) |>
-  summarise(cv.error = mean(valid.error),
-            se.error = sd(valid.error)) |> 
-  ggplot(aes(1/lambda, cv.error)) +
-  geom_line(color = 'gray') + 
-  geom_linerange(aes(ymin = cv.error - se.error,
-                     ymax = cv.error + se.error), size = 2) +
-  geom_linerange(aes(ymin = cv.error - 2 * se.error,
-                     ymax = cv.error + 2 * se.error)) +
-  geom_point(color = 'red', size = 2) + sin_lineas +
-    xlab(paste(expression(1/lambda), " (Complejidad)")) +
-  ylab("Error Validación Cruzada") +
-  scale_x_log10()
-g2
-
 ## Aplicación: The Office ----------------------------------------------------
 
 ratings_raw <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv", show_col_types = FALSE)
 
-  remove_regex <- "[:punct:]|[:digit:]|parts |part |the |and"
+remove_regex <- "[:punct:]|[:digit:]|parts |part |the |and"
 
-  office_ratings <- ratings_raw |>
-    transmute(
-      episode_name = str_to_lower(title),
-      episode_name = str_remove_all(episode_name, remove_regex),
-      episode_name = str_trim(episode_name),
-      imdb_rating
-    )
+office_ratings <- ratings_raw |>
+  transmute(
+    episode_name = str_to_lower(title),
+    episode_name = str_remove_all(episode_name, remove_regex),
+    episode_name = str_trim(episode_name),
+    imdb_rating
+  )
 
-  office_info <- schrute::theoffice |>
-    mutate(
-      season = as.numeric(season),
-      episode = as.numeric(episode),
-      episode_name = str_to_lower(episode_name),
-      episode_name = str_remove_all(episode_name, remove_regex),
-      episode_name = str_trim(episode_name)
-    ) |>
-    select(season, episode, episode_name, director, writer, character)
+office_info <- schrute::theoffice |>
+  mutate(
+    season = as.numeric(season),
+    episode = as.numeric(episode),
+    episode_name = str_to_lower(episode_name),
+    episode_name = str_remove_all(episode_name, remove_regex),
+    episode_name = str_trim(episode_name)
+  ) |>
+  select(season, episode, episode_name, director, writer, character)
 
 characters <- office_info %>%
   count(episode_name, character) %>%
