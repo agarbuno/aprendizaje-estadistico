46dbbba9cf1276a90843fd62ac9fe41ea4c0895e
agarbuno <alfredo.garbuno@itam.mx>
2023-05-01 21:42:05 -0600

    actualiza notas (interp)

--- rscripts/13-interpretability.R
+++ rscripts/13-interpretability.R
@@ -49,12 +49,30 @@ ranger_recipe <-
   step_clean_levels(name, category) |>
   step_impute_knn(depth, height, width)
 
+linear_recipe <-
+  recipe(formula = price ~ ., data = ikea_train) |>
+  step_other(name, category, threshold = 0.01) |>
+  step_clean_levels(name, category) |>
+  step_impute_knn(depth, height, width) |>
+  step_dummy(all_nominal_predictors()) |>
+  step_normalize(all_predictors())
+
 ### Especificación modelo ----------------------------------------------------
+
+linear_spec <-
+  linear_reg(penalty = 1e-3) |>
+  set_mode("regression") |>
+  set_engine("glmnet")
+
+linear_workflow <-
+  workflow() |>
+  add_recipe(linear_recipe) |>
+  add_model(linear_spec)
 
 ranger_spec <-
   rand_forest(trees = 1000) |>
   set_mode("regression") |>
-  set_engine("ranger")
+  set_engine("ranger", importance = "impurity")
 
 ranger_workflow <-
   workflow() |>
@@ -66,10 +84,33 @@ library(doParallel)
 cl <- makePSOCKcluster(all_cores)
 registerDoParallel(cl)
 
+ikea_lm <- linear_workflow |> fit(data = ikea_train)
 ikea_rf <- ranger_workflow |> fit(data = ikea_train)
-ikea_rf
+
+augment(ikea_lm, ikea_test) |>
+  mutate(.linear = .pred,
+         .ranger = predict(ikea_rf, ikea_test) |> pull(.pred)) |>
+  select(c(price, 8:9)) |>
+  pivot_longer(cols = 2:3, names_to = "model", values_to = "predictions") |>
+ggplot(aes(price, predictions)) +
+geom_point(alpha = .4) +
+facet_wrap(~model) +
+geom_abline(intercept = 0, slope = 1, lty = 2) +
+sin_lineas +
+coord_equal()
+
+## (cambio a slides)
 
 library(DALEXtra)
+
+explainer_lm <- 
+  explain_tidymodels(
+    ikea_lm, 
+    data = ikea_train |> select(-price), 
+    y    = ikea_train |> pull(price),
+    label = "linear model",
+    verbose = FALSE
+  )
 
 explainer_rf <- 
   explain_tidymodels(
@@ -84,11 +125,78 @@ set.seed(123)
 mueble <- ikea_test |> sample_n(1)
 mueble
 
-rf_breakdown <- predict_parts(explainer = explainer_rf, new_observation = mueble)
+ikea_lm |> extract_fit_parsnip() |>
+  tidy() |>
+  print(n = 5)
+
+lm_breakdown <- predict_parts(
+  explainer = explainer_lm,
+  new_observation = mueble
+)
+lm_breakdown
+
+rf_breakdown <- predict_parts(
+  explainer = explainer_rf,
+  new_observation = mueble
+)
 rf_breakdown
 
-rf_breakdown |>
-plot()
+rfor_breakdown <- predict_parts(
+  explainer = explainer_rf,
+  new_observation = mueble,
+  order = lm_breakdown$variable_name
+)
+rfor_breakdown
+
+g2 <- rf_breakdown |> plot() + sin_lineas
+g1 <- lm_breakdown |> plot() + sin_lineas
+g3 <- rfor_breakdown |> plot() + sin_lineas
+g1 + g2 + g3
+
+rfin_breakdown <- predict_parts(
+  explainer = explainer_rf,
+  new_observation = mueble,
+  type = "break_down_interactions"
+)
+rfin_breakdown
+
+xgb_spec <-
+  boost_tree(trees = 1000) |>
+  set_mode("regression") |>
+  set_engine("xgboost")
+
+xgb_workflow <-
+  workflow() |>
+  add_recipe(ranger_recipe |> step_dummy(all_nominal_predictors())) |>
+  add_model(xgb_spec)
+
+ikea_xgb <- xgb_workflow |> fit(data = ikea_train)
+
+explainer_xgb <- 
+  DALEX::explain(
+    ikea_xgb, 
+    data = ikea_train |> select(-price), 
+    y    = ikea_train |> pull(price),
+    label = "boosted trees",
+    verbose = FALSE
+  )
+
+library(lime)
+set.seed(108)
+model_type.dalex_explainer <- DALEXtra::model_type.dalex_explainer
+predict_model.dalex_explainer <- DALEXtra::predict_model.dalex_explainer
+
+lime_mueble <- predict_surrogate(
+  explainer = explainer_xgb,
+  new_observation = mueble,
+  n_features = 3,
+  n_permutations = 500,
+  type = "lime"
+)
+
+lime_mueble |> print(width = 85)
+
+lime_mueble |> plot()
 
 set.seed(1801)
 shap_mueble <- 
